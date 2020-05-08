s3rvac/vim-syntax-yara

commit 3559ca1b57d30ee5feedaaf6818c4a04aec93014

Order keywords alphabetically.

syntax/yara.vim
@@ -3,7 +3,7 @@
 " Language:     YARA
 " Maintainer:   Petr Zemek <s3rvac@gmail.com>
 " Home Page:    https://github.com/s3rvac/vim-syntax-yara
-" Last Change:  2018-10-26 06:17:19 +0200
+" Last Change:  2020-01-11 13:15:39 +0100
 "
 " The MIT License (MIT)
 "
@@ -35,7 +35,7 @@ endif
 syntax case match
 
 " Keywords
-syntax keyword yaraKeyword all and any ascii at condition entrypoint false filesize for fullword global import in include int16 int16be int32 int32be int8 int8be matches meta nocase not of or xor private rule strings them true uint16 uint16be uint32 uint32be uint8 uint8be wide contains
+syntax keyword yaraKeyword all and any ascii at condition contains entrypoint false filesize for fullword global import in include int16 int16be int32 int32be int8 int8be matches meta nocase not of or private rule strings them true uint16 uint16be uint32 uint32be uint8 uint8be wide xor
 syntax keyword yaraRuleSection condition meta strings
 
 " Identifiers

commit 403a2b6847e29468dbca972abc3ffb34c9b00b9c

Fix "E395: contains argument not accepted here" when opening a YARA file (#1).
Keywords that are identical to a Vim option have to be defined via match, see [1]:

    Note that when you have a keyword that is the same as an option (even one
    that isn't allowed here), you can not use it. Use a match instead.

Although there are simpler workarounds [2], it will be better to fix this the
proper way and use `match` instead of `keyword`.

This fixes a bug accidentally introduced in commit
3559ca1b57d30ee5feedaaf6818c4a04aec93014 (the original solution worked because
`contains` was the last keyword on the line).

[1] https://vimhelp.org/syntax.txt.html#E789
[2] https://stackoverflow.com/questions/55504057/is-there-any-way-to-highlight-default-vim-keyword-without-using-match

syntax/yara.vim
@@ -3,7 +3,7 @@
 " Language:     YARA
 " Maintainer:   Petr Zemek <s3rvac@gmail.com>
 " Home Page:    https://github.com/s3rvac/vim-syntax-yara
-" Last Change:  2020-05-01 16:00:15 +0200
+" Last Change:  2020-05-08 11:44:44 +0200
 "
 " The MIT License (MIT)
 "
@@ -35,7 +35,10 @@ endif
 syntax case match
 
 " Keywords
-syntax keyword yaraKeyword all and any ascii at base64 base64wide condition contains entrypoint false filesize for fullword global import in include int16 int16be int32 int32be int8 int8be matches meta nocase not of or private rule strings them true uint16 uint16be uint32 uint32be uint8 uint8be wide xor
+syntax keyword yaraKeyword all and any ascii at base64 base64wide condition entrypoint false filesize for fullword global import in include int16 int16be int32 int32be int8 int8be matches meta nocase not of or private rule strings them true uint16 uint16be uint32 uint32be uint8 uint8be wide xor
+" Keywords that are identical to a Vim option have to be defined via match, see
+" https://vimhelp.org/syntax.txt.html#E789.
+syntax match yaraKeyword /\<contains\>/
 syntax keyword yaraRuleSection condition meta strings
 
 " Identifiers